c87b5ac819002892659e31085b79d6f1086b5b61
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-05-15 08:46:54 +0000

    zz

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,4 +1,11 @@
-gp=2.75
-hrs=float(input("enter the number of hrs"))
-rph=hrs*gp
-print("Pay:",rph)
+h=float(input("enter the number of hrs"))
+gp=10.50
+if h<40:
+    pay = h*gp
+    print(' ',pay)
+    
+elif h>40:
+    extra = h - 40
+    gp2 = h*1.5
+    pay1 = h*gp2
+    print(' ',pay1)
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,18 @@
-# Functions
+score = float (input ("Enter Score: ")) 
+if (score >= 0.9 and score <= 1.0):
+     print ('A') 
 
-
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+elif (score >= 0.8 and score<=1.0):
+    print ('B') 
+    
+elif (score >= 0.7 and score<=1.0):
+    print ('C') 
+    
+elif (score >= 0.6 and score<=1.0):
+    print ('D') 
+    
+elif (score < 0.6 and score<=1.0):
+    print ('F') 
+    
+elif (score > 1.0 or score < 0.0):
+    print ('error')
