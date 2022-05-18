af6de406a70e75a96bc27a89448060442148b53d
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-05-18 11:07:18 +0000

    d

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -5,7 +5,4 @@ if h<40:
     print(' ',pay)
     
 elif h>40:
-    extra = h - 40
-    gp2 = h*1.5
-    pay1 = h*gp2
-    print(' ',pay1)
+    pay = (h - 40)
--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,1 +1,16 @@
-
+largest = 0
+smallest = 0
+while True:
+    n = float(input("Enter a number: "))
+    if n == "done":
+        break
+    print(n)
+    try:
+        n = float(n)
+    except:
+            print('invalid input')
+            continue
+largest = max(n)
+smallest = min(n)
+print("Maximum :",largest)
+print("Minimum :",smallest)
