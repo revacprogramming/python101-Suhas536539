6e16c98e3e0d3a73027313055e6d671f553a1772
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-05-30 16:28:35 +0000

    as

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,14 @@
-# Tuples
+fname = input('enter file: ')
+if len(fname) < : fname = 'clown.txt'
+hand = open(fname)
+ 
+ di = dict()
+ for lin in hand:
+     lin = lin.strip()
+     wds = lin.split()
+     for w in wds:
+         di[w] = di.get(w,0) + 1
 
-filename = "dataset/mbox-short.txt"
+print(di)
+x = sorted(di.items())
+print(x)
