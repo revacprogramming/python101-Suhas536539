ab3747cbfe3751d38cdbb056c1db3797020d21f3
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-06-09 15:15:37 +0000

    ss

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,10 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+for lin in handle:
+    wds = lin.split()
+    print(wds)
