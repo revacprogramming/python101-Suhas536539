182472580bb46b49d77695aa9284d6749944354b
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-06-11 16:19:53 +0000

    kjn

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,10 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+for lin in handle:
+    wds = lin.split()
+    print(wds)
