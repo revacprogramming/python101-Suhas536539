0df9aa9f8544bad5c0150b4aa890f6eaffb45bba
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-06-12 08:15:46 +0000

    jdnf

--- ActivitySet#1/netwrk.py
+++ ActivitySet#1/netwrk.py
@@ -39,3 +39,21 @@ def socket_accept():
     print("connection established"+"IP" + address[0] + "Port" + str(address(1)))
     send_command(conn)
     conn.close()
+#send commands 
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024),"utf-8")
+            print(client_response, end="")
+
+def main():
+    create_socket()
+    bind_socket()
+    socket_accept()
+main()
