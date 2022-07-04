35112f63707c98ad7a2dc9c2fd843a7a480910f3
Suhas536539 <suhasnadagowda@gmail.com>
2022-07-04 16:01:56 +0530

    done

--- ActivitySet#2/problem#3.py
+++ ActivitySet#2/problem#3.py
@@ -1,19 +1,35 @@
+#def get_cs():
+ #   """get string input"""
+
+
+#def cs_to_lot(cs):
+ #   """convert connected string to list of strings"""
+
+
+#def main():
+ #   cs = get_cs()
+
+   # lot = cs_to_lot(cs)
+    #print(lot)
+
+
+#if __name__ == '__main__':
+ #   main()
+
+#from pickletools import string1
+
+
+
 
 
 def get_cs():
-    """get string input"""
+    a = str(input("enter a string"))
+    return a
 
 
 def cs_to_lot(cs):
-    """convert connected string to list of strings"""
+
+    
+    
 
 
-def main():
-    cs = get_cs()
-
-    lot = cs_to_lot(cs)
-    print(lot)
-
-
-if __name__ == '__main__':
-    main()
